ae8166e167dee7efc259d6fe8128133783fbaa69
Sergen ORIN <serg_or5@hotmail.com>
2022-06-21 08:00:33 +0300

    basic search functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import Avatar from './components/Avatar';
 import Card from './components/Card';
 
 function App() {
+  const handleSearchSubmit = async (query) => {
+    console.log('Searching', query);
+    const user = await fetch(`https://api.github.com/users/${query}`).then((res) => res.json());
+    console.log(user);
+  };
+
   return (
     <div className="App h-full min-h-screen transition duration-500 dark:bg-cyan-800 ">
       <div className="max-w-2xl mx-auto">
         <Navbar />
-        <SearchInput />
+        <SearchInput onSearchSubmit={handleSearchSubmit} />
         <Card className="mt-4">
           <Avatar imageURL={'https://avatars.githubusercontent.com/u/25180681?v=4'} />
         </Card>
--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,11 +1,15 @@
 import { SearchIcon } from '@heroicons/react/solid';
+import { useState } from 'react';
 
-function SearchInput() {
+function SearchInput({ onSearchSubmit }) {
+  const [query, setQuery] = useState('');
   return (
     <div className="flex space-x-5 mt-4">
       <SearchIcon className="w-8 h-8 dark:text-white transition" />
-      <input type="text" placeholder="Search GitHub username....." className="w-5/6 h-8 px-3 rounded-md bg-gray-400 placeholder-white" />
-      <button className="bg-green-500 px-4 rounded-md font-bold text-white hover:bg-green-600 transition-colors active:bg-green-800 active:text-gray-300">Search</button>
+      <input type="text" placeholder="Search GitHub username....." className="w-5/6 h-8 px-3 rounded-md bg-gray-400 placeholder-white" onChange={(e) => setQuery(e.target.value)} />
+      <button className="bg-green-500 px-4 rounded-md font-bold text-white hover:bg-green-600 transition-colors active:bg-green-800 active:text-gray-300" onClick={() => onSearchSubmit(query)}>
+        Search
+      </button>
     </div>
   );
 }

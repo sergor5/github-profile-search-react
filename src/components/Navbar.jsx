acc0cc87c583d1d89a41f5e9700966f7fbd71867
Sergen ORIN <serg_or5@hotmail.com>
2022-07-03 20:40:15 +0300

    theme updated, svg wave added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,12 @@ import ThemeToggle from './ThemeToggle';
 
 function Navbar() {
   return (
-    <div className="flex items-center justify-between flex-wrap bg-gray-300 dark:bg-gray-800 px-3 py-2 rounded-lg transition">
+    <div
+      className="flex items-center justify-between flex-wrap bg-gray-300 dark:bg-zinc-800 px-3 py-2 rounded-lg transition "
+      style={{
+        boxShadow: 'rgb(0 0 0 / 5%) 0px 1px 3px, rgb(0 0 0 / 5%) 0px 28px 23px -7px, rgb(0 0 0 / 4%) 0px 12px 12px -7px, rgb(255 255 255 / 10%) 0px 1px 0px 1px inset',
+      }}
+    >
       <div className="dark:text-white font-bold transition">Github User Search</div>
       <ThemeToggle />
     </div>
--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -7,7 +7,13 @@ function SearchInput({ onSearchSubmit }) {
     <div className="flex space-x-5 mt-4">
       <SearchIcon className="w-8 h-8 dark:text-white transition" />
       <input type="text" placeholder="Search GitHub username....." className="w-5/6 h-8 px-3 rounded-md bg-gray-400 placeholder-white" onChange={(e) => setQuery(e.target.value)} />
-      <button className="bg-green-500 px-4 rounded-md font-bold text-white hover:bg-green-600 transition-colors active:bg-green-800 active:text-gray-300" onClick={() => onSearchSubmit(query)}>
+      <button
+        style={{
+          boxShadow: 'rgb(0 0 0 / 5%) 0px 1px 3px, rgb(0 0 0 / 5%) 0px 28px 23px -7px, rgb(0 0 0 / 4%) 0px 12px 12px -7px, rgb(255 255 255 / 10%) 0px 1px 0px 1px inset',
+        }}
+        className="bg-emerald-400 px-4 rounded-md font-bold text-white hover:bg-emerald-600 transition-colors active:bg-emerald-800 active:text-gray-300"
+        onClick={() => onSearchSubmit(query)}
+      >
         Search
       </button>
     </div>

feb016fe0bc8e75448518c6cf5942a30efbe97e4
Sergen ORIN <serg_or5@hotmail.com>
2022-07-03 23:25:41 +0300

    small theme updates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 function Card({ children, className = '' }) {
-  return <div className={`flex items-center flex-wrap bg-gray-300 dark:bg-gray-800 dark:text-white  px-3 py-2 rounded-lg transition ${className}`}>{children}</div>;
+  return <div className={`flex items-center flex-wrap bg-gray-300 dark:bg-gray-800 dark:text-white px-3 py-2 rounded-lg transition ${className}`}>{children}</div>;
 }
 
 export default Card;
--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -6,7 +6,12 @@ function SearchInput({ onSearchSubmit }) {
   return (
     <div className="flex space-x-5 mt-4">
       <SearchIcon className="w-8 h-8 dark:text-white transition" />
-      <input type="text" placeholder="Search GitHub username....." className="w-5/6 h-8 px-3 rounded-md bg-gray-400 placeholder-white" onChange={(e) => setQuery(e.target.value)} />
+      <input
+        type="text"
+        placeholder="Search GitHub username....."
+        className="w-5/6 h-8 px-3 rounded-md bg-zinc-400 dark:bg-zinc-600 placeholder-white transition"
+        onChange={(e) => setQuery(e.target.value)}
+      />
       <button
         style={{
           boxShadow: 'rgb(0 0 0 / 5%) 0px 1px 3px, rgb(0 0 0 / 5%) 0px 28px 23px -7px, rgb(0 0 0 / 4%) 0px 12px 12px -7px, rgb(255 255 255 / 10%) 0px 1px 0px 1px inset',

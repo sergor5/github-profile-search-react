46fdbbeccce52201dec46777c5890699c0624826
Sergen ORIN <serg_or5@hotmail.com>
2022-07-03 23:47:15 +0300

    hover states, search input enter event, avatar border fix, new layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,14 @@
 function Card({ children, className = '' }) {
-  return <div className={`flex items-center flex-wrap bg-gray-300 dark:bg-gray-800 dark:text-white px-3 py-2 rounded-lg transition ${className}`}>{children}</div>;
+  return (
+    <div
+      className={`flex items-center flex-wrap bg-gray-300 dark:bg-gray-800 dark:text-white px-3 py-2 rounded-lg transition ${className}`}
+      style={{
+        boxShadow: 'rgb(0 0 0 / 5%) 0px 1px 3px, rgb(0 0 0 / 5%) 0px 28px 23px -7px, rgb(0 0 0 / 4%) 0px 12px 12px -7px, rgb(255 255 255 / 10%) 0px 1px 0px 1px inset',
+      }}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default Card;

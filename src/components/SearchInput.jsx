90899fc5576eb2a785af7c95d93fb45e8847f52b
Sergen ORIN <serg_or5@hotmail.com>
2022-07-04 11:12:36 +0300

    basic error handling, input animation

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -2,8 +2,12 @@ import { SearchIcon } from '@heroicons/react/solid';
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
-function SearchInput({ onSearchSubmit, isSearching = true }) {
+function SearchInput({ onSearchSubmit, isSearching = true, hasErrors = false }) {
   const [query, setQuery] = useState('');
+  const inputAnimation = {
+    initial: { x: 0, transition: { duration: 0.6 } },
+    shake: { x: [0, 7, 0, -7, 0, 7, 0], transition: { repeat: 0, duration: 0.2, type: 'spring', damping: 10, stiffness: 10 } },
+  };
   return (
     <div className="flex justify-between space-x-5 mt-4">
       <AnimatePresence>
@@ -25,15 +29,18 @@ function SearchInput({ onSearchSubmit, isSearching = true }) {
           </div>
         )}
       </AnimatePresence>
-      <input
-        type="text"
-        placeholder="Search GitHub username....."
-        className="w-5/6 h-8 px-3 rounded-md bg-zinc-400 dark:bg-zinc-600 placeholder-white transition text-black dark:text-white outline-none hover:-translate-y-0.5 focus:translate-y-0"
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.code == 'Enter') onSearchSubmit(query);
-        }}
-      />
+      <motion.div className="w-5/6" animate={hasErrors ? 'shake' : 'initial'} variants={inputAnimation}>
+        <input
+          type="text"
+          placeholder="Search GitHub username....."
+          style={{ border: hasErrors ? '1px solid red' : '' }}
+          className="w-full h-8 px-3 rounded-md bg-zinc-400 dark:bg-zinc-600 placeholder-white transition text-black dark:text-white outline-none hover:-translate-y-0.5 focus:translate-y-0"
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.code == 'Enter') onSearchSubmit(query);
+          }}
+        />
+      </motion.div>
       <button
         style={{
           boxShadow: 'rgb(0 0 0 / 5%) 0px 1px 3px, rgb(0 0 0 / 5%) 0px 28px 23px -7px, rgb(0 0 0 / 4%) 0px 12px 12px -7px, rgb(255 255 255 / 10%) 0px 1px 0px 1px inset',
